0698e2bdca33acd0cdd22ddef2c786383224ccf5
joshna-22 <bollavaramjoshna@gmail.com>
2025-09-14 02:04:51 +0530

    Create ParentPortal.jsx

---
+++ src/components/ParentPortal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ParentPortal() {
+  return (
+    <div>
+      <h2>Parent Portal</h2>
+      {/* Add parent-specific dashboard and features here */}
+    </div>
+  );
+}
