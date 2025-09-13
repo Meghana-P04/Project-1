69fccb8e3160c10ad48d9cf6b6568a023f16e9e2
joshna-22 <bollavaramjoshna@gmail.com>
2025-09-14 02:11:04 +0530

    Create Earthquakesim.jsx

---
+++ src/components/Simulation/Earthquakesim.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EarthquakeSim() {
+  return (
+    <div>
+      <h2>Earthquake Simulation</h2>
+      {/* Add earthquake simulation logic or animation here */}
+    </div>
+  );
+}
