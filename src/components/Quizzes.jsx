b596e937495a4d253c1ff6353b0b9d545ba18a4d
joshna-22 <bollavaramjoshna@gmail.com>
2025-09-14 02:10:14 +0530

    Create Quizzes.jsx

---
+++ src/components/Quizzes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Quizzes() {
+  return (
+    <div>
+      <h2>Quizzes</h2>
+      {/* Add quiz questions and answer submission logic here */}
+    </div>
+  );
+}
