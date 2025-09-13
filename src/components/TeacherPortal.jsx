46c941ea5461009a2facc3247d0fbfbda571c728
joshna-22 <bollavaramjoshna@gmail.com>
2025-09-14 02:04:11 +0530

    Create TeacherPortal.jsx

---
+++ src/components/TeacherPortal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TeacherPortal() {
+  return (
+    <div>
+      <h2>Teacher Portal</h2>
+      {/* Add teacher-specific dashboard and features here */}
+    </div>
+  );
+}import React from "react";
+
+export default function TeacherPortal() {
+  return (
+    <div>
+      <h2>Teacher Portal</h2>
+      {/* Add teacher-specific dashboard and features here */}
+    </div>
+  );
+}
