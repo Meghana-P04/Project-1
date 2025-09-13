cd4f4e43479ce631fbceda9b5e38bcd2450ddee5
joshna-22 <bollavaramjoshna@gmail.com>
2025-09-14 02:11:34 +0530

    Create Floodsim.jsx

---
+++ src/components/Simulation/Floodsim.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FloodSim() {
+  return (
+    <div>
+      <h2>Flood Simulation</h2>
+      {/* Add flood simulation logic or animation here */}
+    </div>
+  );
+}
