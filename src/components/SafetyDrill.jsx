1dcfd58c29fe461b07df0a4834f92a8e9c52287a
joshna-22 <bollavaramjoshna@gmail.com>
2025-09-14 02:09:37 +0530

    Create SafetyDrill.jsx

---
+++ src/components/SafetyDrill.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SafetyDrill() {
+  return (
+    <div>
+      <h2>Safety Drill</h2>
+      {/* Add safety drill details and participation logic here */}
+    </div>
+  );
+}
